40bb2bd4d7c10cef2a1eb70fd80509d55082e374
ViridianMelody <mlove@anaconda.com>
2026-01-27 14:22:49 -0800

    Fix CFLAGS update

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 #export OPENSSL_DIR = "$PREFIX"
 if [ $( uname -s ) == "Linux" ]; then
   # This flag is needed for tree-sitter on linux
-  CFLAGS+= " -D_BSD_SOURCE"
+  export CFLAGS="${CFLAGS} -D_BSD_SOURCE"
 fi
 cargo install --locked --root $PREFIX --no-track --path crates/pixi
 cargo-bundle-licenses --format yaml --output "$SRC_DIR/THIRDPARTY.yml"
