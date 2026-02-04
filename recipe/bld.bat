bb101b2e3e1ea58facc08079aebee234837fa06d
Jamie Robertson <jamierobertsongames@gmail.com>
2026-02-04 15:47:03 +0000

    ensure show path in windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,9 @@
 set CARGO_NET_GIT_FETCH_WITH_CLI=true
+
+subst Y: "%SRC_DIR%"
+set CARGO_HOME=Y:\.cargo
+
 cargo install --locked --root %PREFIX% --no-track --path crates/pixi
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%/THIRDPARTY.yml"
+
+subst Y: /d
