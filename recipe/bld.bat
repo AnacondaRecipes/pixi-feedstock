27e8d04cd251ce51c8bc7699216593a8702c633c
Dan Yeaw <dan@yeaw.me>
2026-06-01 14:12:57 -0400

    Use Anaconda default channels

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,9 @@ set CARGO_NET_GIT_FETCH_WITH_CLI=true
 subst Y: "%SRC_DIR%"
 set CARGO_HOME=Y:\.cargo
 
+rem Anaconda default channels (compile-time via option_env! in pixi_consts)
+set PIXI_DEFAULT_CHANNELS=https://repo.anaconda.com/pkgs/main,https://repo.anaconda.com/pkgs/msys2
+
 cargo install --locked --root %PREFIX% --no-track --path crates/pixi
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%/THIRDPARTY.yml"
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,9 @@ if [ $( uname -s ) == "Linux" ]; then
   # This flag is needed for tree-sitter on linux
   export CFLAGS="${CFLAGS} -D_BSD_SOURCE"
 fi
+
+# Anaconda default channels (compile-time via option_env! in pixi_consts)
+export PIXI_DEFAULT_CHANNELS="https://repo.anaconda.com/pkgs/main"
+
 cargo install --locked --root $PREFIX --no-track --path crates/pixi
 cargo-bundle-licenses --format yaml --output "$SRC_DIR/THIRDPARTY.yml"
